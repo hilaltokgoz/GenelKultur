4d0fc9a1f77f9d9505227e2bbed629f24aeaf8d9
Hilal <hilaltokgoz98@gmail.com>
2021-05-21 23:03:38 +0300

    oMuBuMu buton renkleri düzeltildi

--- app/src/main/java/com/genelkultur/OMuBuMuFragment1.kt
+++ app/src/main/java/com/genelkultur/OMuBuMuFragment1.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.ColorLong
+import androidx.core.content.ContextCompat
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -40,6 +41,8 @@ class OMuBuMuFragment1 : Fragment() {
         getDataTarihOmuBumu()
         iv_tarih_refresh_oBu.setOnClickListener {
             getDataTarihOmuBumu()
+            radioButton.setBackgroundColor(ContextCompat.getColor(requireContext(),R.color.set_sail_sampange))
+            radioButton2.setBackgroundColor(ContextCompat.getColor(requireContext(),R.color.set_sail_sampange))
         }
 
 
